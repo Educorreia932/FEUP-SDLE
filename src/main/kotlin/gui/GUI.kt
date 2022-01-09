611877a905f71e7ca8b34a9d6c03995d77b79bc0
Eduardo Correia <skelozard1@gmail.com>
2022-01-09 16:24:54 +0000

    Created a Login form

--- src/main/kotlin/gui/GUI.kt
+++ src/main/kotlin/gui/GUI.kt
@@ -1,37 +1,10 @@
 package gui
 
-import java.awt.Container
 import java.awt.EventQueue
-import javax.swing.*
 
-
-class GUI : JFrame() {
-    init {
-        defaultCloseOperation = EXIT_ON_CLOSE
-        setSize(800, 600)
-        setLocationRelativeTo(null)
-
-        val pane = getRootPane()
-
-        pane.layout = BoxLayout(pane, BoxLayout.Y_AXIS)
-
-        addButton("Button 1", pane);
-        addButton("Button 2", pane);
-        addButton("Button 3", pane);
-        addButton("Long-Named Button 4", pane);
-        addButton("5", pane);
-    }
-
-    private fun addButton(text: String, container: Container) {
-        val button = JButton(text)
-        
-        button.alignmentX = CENTER_ALIGNMENT
-        container.add(button)
-    }
-}
 
 private fun createAndShowGUI() {
-    val frame = GUI()
+    val frame = Login()
 
     frame.isVisible = true
 }
---
+++ src/main/kotlin/gui/Login.kt
@@ -0,0 +1,90 @@
+package gui
+
+import java.awt.Container
+import javax.swing.*
+import javax.swing.text.AbstractDocument
+import javax.swing.text.AttributeSet
+import javax.swing.text.BadLocationException
+import javax.swing.text.DocumentFilter
+
+
+class Login : JFrame() {
+    private class UsernameFilter : DocumentFilter() {
+        override fun insertString(
+            fb: FilterBypass?,
+            offset: Int,
+            string: String?,
+            attr: AttributeSet?
+        ) {
+            if (offset >= 1) {
+                super.insertString(fb, offset, string, attr)
+            }
+        }
+
+        override fun remove(fb: FilterBypass, offset: Int, length: Int) {
+            if (offset >= 1) {
+                super.remove(fb, offset, length)
+            }
+        }
+
+        @kotlin.Throws(BadLocationException::class)
+        override fun replace(
+            fb: FilterBypass?,
+            offset: Int,
+            length: Int,
+            text: String?,
+            attrs: AttributeSet?
+        ) {
+            if (offset >= 1) {
+                super.replace(fb, offset, length, text, attrs)
+            }
+        }
+    }
+
+    private var container: Container = contentPane
+    private var userLabel = JLabel("Username")
+    private var passwordLabel = JLabel("Password")
+    private var userTextField = JTextField("@")
+    private var passwordField = JPasswordField()
+    private var loginButton = JButton("Login")
+    private var resetButton = JButton("Reset")
+    private var showPassword = JCheckBox("Show Password")
+
+    init {
+        title = "Tulicreme | Login"
+        isVisible = true
+        defaultCloseOperation = EXIT_ON_CLOSE
+        isResizable = false
+
+        setBounds(10, 10, 370, 600);
+        setLayoutManager()
+        setLocationAndSize()
+        addComponentsToContainer()
+    }
+
+    private fun setLayoutManager() {
+        container.layout = null
+    }
+
+    private fun setLocationAndSize() {
+        userLabel.setBounds(50, 150, 100, 30)
+        passwordLabel.setBounds(50, 220, 100, 30)
+        userTextField.setBounds(150, 150, 150, 30)
+        passwordField.setBounds(150, 220, 150, 30)
+        showPassword.setBounds(150, 250, 150, 30)
+        loginButton.setBounds(50, 300, 100, 30)
+        resetButton.setBounds(200, 300, 100, 30)
+    }
+
+    private fun addComponentsToContainer() {
+        (userTextField.document as AbstractDocument).documentFilter = UsernameFilter()
+
+        container.add(userLabel)
+        container.add(passwordLabel)
+        container.add(userTextField)
+        container.add(passwordField)
+        container.add(showPassword)
+        container.add(loginButton)
+        container.add(resetButton)
+    }
+}
